469fb8c796261bef51b31286717b187dca8493a1
souheil <souheilmansouri06@gmail.com>
2026-06-02 18:50:14 +0100

    add input decorator to user component @Input and the required feature on it

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component,computed,signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
-  const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,12 +8,14 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-selectedUser=signal(DUMMY_USERS[randomIndex]);
-imagePath=computed(()=>'assets/users/'+ this.selectedUser().avatar)
+@Input({required:true}) avatar!:string
+@Input({required:true}) name!:string
 
+get imagePath(){
+  return 'assets/users/'+ this.avatar
+}
 onSelectUser(){
-  const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
-  this.selectedUser.set(DUMMY_USERS[randomIndex]) 
+
 }
 
 }
