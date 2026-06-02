185b925d1a5205773710b5930e1103b3bdabeff2
souheil <souheilmansouri06@gmail.com>
2026-06-02 19:45:58 +0100

    how to work with output function instead of output decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input,Output } from '@angular/core';
+import { Component, EventEmitter, Input,Output,output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,13 +11,14 @@ export class UserComponent {
 @Input({required:true}) id!:string
 @Input({required:true}) avatar!:string
 @Input({required:true}) name!:string
-@Output() select= new EventEmitter()
+// @Output() select= new EventEmitter()
+select=output<string>();
 
 get imagePath(){
   return 'assets/users/'+ this.avatar
 }
 onSelectUser(){
-this.select.emit(this.id)
+this.select.emit(this.id);
 }
 
 }
